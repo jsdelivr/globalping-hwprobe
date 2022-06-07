974614af18a36084a657de59955c405170f236c7
Nuno Felicio <nuno.felicio@gmail.com>
2022-06-07 21:35:48 +0100

    Fixes and improvements  (#2)
    * Don't try to update the docker image on startup
    
    * Turn off LLMR
    
    * Update jsdelivr-startWorld.sh
    
    Co-authored-by: Nuno Felicio <nuno.felicio@trotiltek.com>

--- meta-jsdelivr/recipes-jsdelivr/jsdelivr-scripts/files/jsdelivr-firstBoot.sh
+++ meta-jsdelivr/recipes-jsdelivr/jsdelivr-scripts/files/jsdelivr-firstBoot.sh
@@ -8,6 +8,7 @@ then
     cp /etc/machine-id /tmp/once/etc
     touch /tmp/once/etc/jsdlvr_first_boot_flag
     echo "VendorClassIdentifier=globalping-probe" >> /tmp/once/lib/systemd/network/80-wired.network
+    echo "LLMNR=no" >> /tmp/once/etc/systemd/resolved.conf
     umount /tmp/once
     echo "mkpart primary ext2 700MB 1200MB" | /usr/sbin/parted /dev/mmcblk0
     sync
