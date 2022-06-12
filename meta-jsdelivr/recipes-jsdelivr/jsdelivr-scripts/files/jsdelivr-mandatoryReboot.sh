a588fa920c973a05398430496c316f976e74692f
Nuno Felicio <nuno.felicio@gmail.com>
2022-06-12 18:13:14 +0100

    Add a random offset to the mandatory reboot period (#4)
    Co-authored-by: Nuno Felicio <nuno.felicio@trotiltek.com>

--- meta-jsdelivr/recipes-jsdelivr/jsdelivr-scripts/files/jsdelivr-mandatoryReboot.sh
+++ meta-jsdelivr/recipes-jsdelivr/jsdelivr-scripts/files/jsdelivr-mandatoryReboot.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
-DAYS=3
-((MANDATORY_REBOOT_PERIOD= 24*60*60*$DAYS))
+#!/bin/bash
 
-echo "Mandatory reboot period is $MANDATORY_REBOOT_PERIOD seconds"
-echo "Sleeping...."
+DAYS=3
+RANDOM_OFFSET_DAYS=2
+
+
+
+((RANDOM_OFFSET_MAX= 24*60*60*$RANDOM_OFFSET_DAYS))
+
+((MANDATORY_REBOOT_PERIOD_BASE= 24*60*60*$DAYS))
+
+((RANDOM_OFFSET= $RANDOM % $RANDOM_OFFSET_MAX ))
+
+((MANDATORY_REBOOT_PERIOD= $MANDATORY_REBOOT_PERIOD_BASE + $RANDOM_OFFSET ))
+
+
+echo "Mandatory reboot random offset is $RANDOM_OFFSET seconds" > /dev/tty4
+echo "Mandatory reboot period base is $MANDATORY_REBOOT_PERIOD_BASE seconds" > /dev/tty4
+echo "Mandatory reboot period is $MANDATORY_REBOOT_PERIOD seconds" > /dev/tty4
+
+echo "Sleeping...." > /dev/tty4
+
 sleep $MANDATORY_REBOOT_PERIOD
 
-echo "It's time...."
+echo "It's time...." > /dev/tty4
+
 
 killall jsdelivr_systemWatchdog
 docker kill $(docker ps -q)
 reboot &
-echo "Waiting for watchdog reboot or normal system reboot"
-while :; do  sleep 2; done
 
+
+echo "Waiting for watchdog reboot or normal system reboot" > /dev/tty4
+while :; do  sleep 2; done
 
 
