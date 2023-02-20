722c44e97937578f94d8cade326f4e9c607d5cd1
Nuno Felicio <nuno.felicio@gmail.com>
2023-02-20 23:41:52 +0000

    Add the NTP.ORG ntp server pool as the first option

--- meta-jsdelivr/recipes-jsdelivr/jsdelivr-scripts/files/jsdelivr-firstBoot.sh
+++ meta-jsdelivr/recipes-jsdelivr/jsdelivr-scripts/files/jsdelivr-firstBoot.sh
@@ -9,6 +9,7 @@ then
     touch /tmp/once/etc/jsdlvr_first_boot_flag
     echo "VendorClassIdentifier=globalping-probe" >> /tmp/once/lib/systemd/network/80-wired.network
     echo "LLMNR=no" >> /tmp/once/etc/systemd/resolved.conf
+    echo "NTP=pool.ntp.org" >> /tmp/once/etc/systemd/timesyncd.conf
     umount /tmp/once
     echo "mkpart primary ext2 700MB 1200MB" | /usr/sbin/parted /dev/mmcblk0
     sync
