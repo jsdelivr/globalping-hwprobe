3e8ee96582568d9a690095d544af9b2fda66806f
Nuno Felicio <nuno.felicio@invenco.com>
2024-12-02 23:39:12 +0000

    Add GP_PROBE UUID V4 (doesn't survive sd card reflash)

--- meta-jsdelivr/recipes-jsdelivr/jsdelivr-scripts/files/jsdelivr-firstBoot.sh
+++ meta-jsdelivr/recipes-jsdelivr/jsdelivr-scripts/files/jsdelivr-firstBoot.sh
@@ -6,6 +6,27 @@ then
     mount /dev/mmcblk0p2 /tmp/once
     mount -o remount,rw /dev/mmcblk0p2 /tmp/once
     cp /etc/machine-id /tmp/once/etc
+    export MUID=$(dd if=/dev/urandom bs=16 count=1 2>/dev/null | od -An -t x1 | \
+    awk '{
+	# Read bytes into an array
+	for(i=1; i<=NF; i++) {
+    	    byte = strtonum("0x"$i)
+    	    bytes[i] = byte
+	}
+	bytes[7] = and(bytes[7], 0x0F)
+	bytes[7] = or(bytes[7], 0x40)
+	bytes[9] = and(bytes[9], 0x3F)
+	bytes[9] = or(bytes[9], 0x80)
+	printf("%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x\n",
+    	    bytes[1], bytes[2], bytes[3], bytes[4],
+    	    bytes[5], bytes[6],
+    	    bytes[7], bytes[8],
+    	    bytes[9], bytes[10],
+    	    bytes[11], bytes[12], bytes[13], bytes[14], bytes[15], bytes[16])
+	}'
+    )
+
+    echo "GP_PROBE_UUID=$MUID" > /tmp/once/etc/muid.data
     echo "VendorClassIdentifier=globalping-probe" >> /tmp/once/lib/systemd/network/80-wired.network
     echo "LLMNR=no" >> /tmp/once/etc/systemd/resolved.conf
     echo "NTP=pool.ntp.org" >> /tmp/once/etc/systemd/timesyncd.conf
